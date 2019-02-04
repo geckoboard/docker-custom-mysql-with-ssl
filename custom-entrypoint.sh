e2da91ccf8f17407ab05409ca374413f5fe21f8c
Kyrill Poole <kyrill@geckoboard.com>
2019-02-04 16:03:45 +0000

    Fix CA certificate location

--- custom-entrypoint.sh
+++ custom-entrypoint.sh
@@ -3,10 +3,10 @@
 if [ "$SERVER_KEY" ]; then
   echo $SERVER_KEY | sed "s/\\$/\n/g" | sed "s/^ //g" >/etc/mysql/server.key
   echo $SERVER_CERT | sed "s/\\$/\n/g" | sed "s/^ //g" >/etc/mysql/server.crt
-  echo $CA_CERT | sed "s/\\$/\n/g" | sed "s/^ //g" >/etc/mysql/CA.crt
+  echo $CA_CERT | sed "s/\\$/\n/g" | sed "s/^ //g" >/etc/mysql/ca.crt
   export MYSQLD_SSL_KEY=/etc/mysql/server.key
   export MYSQLD_SSL_CERT=/etc/mysql/server.crt
-  export MYSQLD_SSL_CA=/etc/mysql/CA.crt
+  export MYSQLD_SSL_CA=/etc/mysql/ca.crt
 fi
 
 echo -e "[mysqld]\n" >/etc/mysql/conf.d/custom.cnf
